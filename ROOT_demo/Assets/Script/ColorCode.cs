b334f4ba54e6fb0ce8e2123554e4849517ef84ff
syx007 <suyuxuan007@hotmail.com>
2020-09-08 21:40:01 +0800

    update localization csv and ColorCode
    please let me know if you understand the colorcode summary

--- ROOT_demo/Assets/Script/ColorCode.cs
+++ ROOT_demo/Assets/Script/ColorCode.cs
@@ -4,38 +4,20 @@ namespace ROOT
 {
     /// <summary>
     /// This class stores all the color code(HEX string) that have been used in this game
+    /// the naming schema for this class would be:
+    ///     ROOT_{color_environment}_{description}_{color name}
+    ///{color environment} means what kind of scenario that color is used in, like used in SHOP or text
+    ///{description} please try us use one word to describe the color meaning the color environment/scenario
+    ///     For example, if you use ROOT_DATA_NETWORK_BLUE to mark some text, the NETWORK describe the text would
+    ///     have network data meaning in the DATA scenario.
+    ///{color name} the color you have used
     /// </summary>
-    public static class ColorCode
+    public static class ColorName
     {
-        /// <summary>
-        /// This method would return red color code
-        /// </summary>
-        public static String getRedColorCode { get { return ColorName.RED; } }
-        /// <summary>
-        /// This method would return purple color code
-        /// </summary>
-        public static String getPurpleColorCode { get { return ColorName.PURPLE; } }
-        /// <summary>
-        /// This method would return blue color code
-        /// </summary>
-        public static String getBlueColorCode { get { return ColorName.BLUE; } }
-        /// <summary>
-        /// This method would return green color code
-        /// </summary>
-        public static String getGreenColorCode { get { return ColorName.GREEN; } }
-        /// <summary>
-        /// This method would return orange color code
-        /// </summary>
-        public static String getOrangeColorCode { get { return ColorName.ORANGE; } }
-
-    }
-
-    static class ColorName
-    {
-        public const string RED = "#FF1515";
-        public const string PURPLE = "#9D33FF";
-        public const string BLUE = "#2D39FF";
-        public const string GREEN = "#2DFF3F";
-        public const string ORANGE = "#FF5733";
+        public const string ROOT_EVENT_DISASTER_RED = "#FF1515";
+        public const string ROOT_SHOP_CHEAP_PURPLE = "#9D33FF";
+        public const string ROOT_DATA_NETWORK_BLUE = "#2D39FF";
+        public const string ROOT_DATA_GENERAL_GREEN = "#2DFF3F";
+        public const string ROOT_TEXT_EMPHASIZE_ORANGE = "#FF5733";
     }
 }
