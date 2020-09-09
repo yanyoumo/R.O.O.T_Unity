51db18b1f6a8271a03814dd8c8d0d31d80baf443
yanyoumo <yanyoumo@gmail.com>
2020-09-09 10:51:29 +0800

    制作AnalogLED

---
+++ ROOT_demo/Assets/AnalogLEDArray.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using ROOT;
+using UnityEngine;
+
+namespace ROOT
+{
+    public class AnalogLEDArray : LEDArray
+    {
+        private const int MaxVal = 50;
+        private const float Starting = -3.85f;
+        public MeshRenderer OnMesh;
+        public Transform OnMeshRoot;
+
+        void Awake()
+        {
+            OnMesh.material.color = LEDColor;
+        }
+
+        public override int Val
+        {
+            set
+            {
+                float normalizedVal = value / (float) MaxVal;
+                normalizedVal = Mathf.Clamp01(normalizedVal);
+                normalizedVal = Mathf.Pow(normalizedVal, 1/1.5f);
+                float posX = Starting + Mathf.Abs(Starting) * normalizedVal;
+                OnMeshRoot.transform.localScale = new Vector3(normalizedVal, 1.0f, 1.0f);
+                OnMeshRoot.transform.localPosition = new Vector3(posX, 0.0f, 0.0f);
+            }
+        }
+
+        void Update()
+        {
+            //Val = (Time.frameCount / 30);
+        }
+    }
+}
