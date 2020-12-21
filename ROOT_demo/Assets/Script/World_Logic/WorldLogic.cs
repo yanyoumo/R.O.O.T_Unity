fc07dcda6150cf372aebc3e102c19d6b9c862e5b
Digong Jiang <jiangdigong@gmail.com>
2020-12-21 15:03:27 +0800

    bug fix

--- ROOT_demo/Assets/Script/World_Logic/WorldLogic.cs
+++ ROOT_demo/Assets/Script/World_Logic/WorldLogic.cs
@@ -686,7 +686,6 @@ namespace ROOT
                 }
                 holdPos = null;
                 holdTime = 0;
-                startPos = null;
             }
             //双击的时间阈值是0.3s
             else if (idle == false && _isSinglePress && Time.time - pressTime >= 0.3)
@@ -706,7 +705,6 @@ namespace ROOT
                 holdPos = null;
                 holdTime = 0;
                 _pressedObj = null;
-                startPos = null;
             }
         }
 
