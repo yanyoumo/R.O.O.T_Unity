3af43ae3f69690ab1e7b0337374b01f707fa0155
yanyoumo <yanyoumo@gmail.com>
2020-12-25 17:01:12 +0800

    继续调整Unit解耦

--- ROOT_demo/Assets/Resources/Signal/Script/Bases/UnitSignalCoreBase.cs
+++ ROOT_demo/Assets/Resources/Signal/Script/Bases/UnitSignalCoreBase.cs
@@ -15,8 +15,29 @@ namespace ROOT
         [ReadOnly] public int HardDiskVal;
         [ReadOnly] public bool InHddGrid;
         [ReadOnly] public bool InHddSignalGrid;
-        [ReadOnly] public int ServerDepth;
-        [ReadOnly] public bool InServerGrid;
+
+        /// <summary>
+        /// 记录服务器信号深度的变量，和服务器相连的Network该数值应该为1.
+        /// 可以作为中间量、即使不处于最长序列该值不必清除。
+        /// </summary>
+        [ReadOnly] public int ServerDepth; //for scoring purpose
+
+        /// <summary>
+        /// 标记一次计分后，本单元是否处于必要最长序列中。不处于的需要显式记为false。
+        /// </summary>
+        [ReadOnly] public bool InServerGrid; //for scoring purpose
+
+        /// <summary>
+        /// 具体显示LED的field，即，最接近服务器的该数值应为全部深度，最枝端的显示值需要为1。
+        /// </summary>
+        public int NetworkVal => ServerDepth;
+
+        void Awake()
+        {
+            Visited = false;
+            InServerGrid = false;
+            InHddGrid = false;
+        }
 
         public int CalScore()
         {
--- ROOT_demo/Assets/Script/_Common/Unit.cs
+++ ROOT_demo/Assets/Script/_Common/Unit.cs
@@ -315,30 +315,51 @@ namespace ROOT
             _coreMatNameDic.Add(CoreType.HQ, GlobalResourcePath.UNIT_HQ_MAT_NAME); //TODO HQ的核心还没有实际材质
         }
 
-        [ReadOnly] public RotationDirection SignalFromDir;
-        [ReadOnly] public bool Visited { get; set; }//for scoring purpose: dequeue
-        [ReadOnly] public bool Visiting { get; set; }//for scoring purpose: enqueue
-        [ReadOnly] public int HardDiskVal; //for scoring purpose
-        [ReadOnly] public bool InHddGrid { get; set; } //for scoring purpose
-        [ReadOnly] public bool InHddSignalGrid; //for scoring purpose
+        #region 临时转译
+        //现在这里只是和SignalCoreBase的转译字段，彻底转过来后就彻底移除。
+        //现在信号相关的数据已经从单元本身（硬件）中移除出去了。
+        public RotationDirection SignalFromDir
+        {
+            get => SignalCore.SignalFromDir;
+            set => SignalCore.SignalFromDir = value;
+        }
+        public bool Visited
+        {
+            get => SignalCore.Visited;
+            set => SignalCore.Visited = value;
+        }
+        public bool Visiting
+        {
+            get => SignalCore.Visiting;
+            set => SignalCore.Visiting = value;
+        }
+        public int HardDiskVal
+        {
+            get => SignalCore.HardDiskVal;
+            set => SignalCore.HardDiskVal = value;
+        }
+        public bool InHddGrid
+        {
+            get => SignalCore.InHddGrid;
+            set => SignalCore.InHddGrid = value;
+        }
+        public bool InHddSignalGrid
+        {
+            get => SignalCore.InHddSignalGrid;
+            set => SignalCore.InHddSignalGrid = value;
+        }
 
-        #region 服务器计分
-
-        /// <summary>
-        /// 记录服务器信号深度的变量，和服务器相连的Network该数值应该为1.
-        /// 可以作为中间量、即使不处于最长序列该值不必清除。
-        /// </summary>
-        [ReadOnly] public int ServerDepth; //for scoring purpose
-
-        /// <summary>
-        /// 标记一次计分后，本单元是否处于必要最长序列中。不处于的需要显式记为false。
-        /// </summary>
-        [ReadOnly] public bool InServerGrid; //for scoring purpose
-
-        /// <summary>
-        /// 具体显示LED的field，即，最接近服务器的该数值应为全部深度，最枝端的显示值需要为1。
-        /// </summary>
-        public int NetworkVal => ServerDepth;
+        public int ServerDepth
+        {
+            get => SignalCore.ServerDepth;
+            set => SignalCore.ServerDepth = value;
+        }
+        public bool InServerGrid
+        {
+            get => SignalCore.InServerGrid;
+            set => SignalCore.InServerGrid = value;
+        }
+        public int NetworkVal => SignalCore.NetworkVal;
 
         #endregion
 
@@ -479,9 +500,6 @@ namespace ROOT
             InitConnector(_localWestConnector, lWSide);
             InitConnector(_localSouthConnector, lSSide);
 
-            Visited = false;
-            InServerGrid = false;
-            InHddGrid = false;
             GameBoard = gameBoard;
 
             Tier = tier;
