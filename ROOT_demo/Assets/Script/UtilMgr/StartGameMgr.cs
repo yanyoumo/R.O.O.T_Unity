f8c918907428230f17c9589bd3dc93dad6018fef
Yan Youmo <1737612133@qq.com>
2021-01-27 22:34:51 +0800

    Update StartGameMgr.cs

--- ROOT_demo/Assets/Script/UtilMgr/StartGameMgr.cs
+++ ROOT_demo/Assets/Script/UtilMgr/StartGameMgr.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
@@ -136,30 +138,240 @@ namespace ROOT
         
         int doSearch_BasicTechniqueB()
         {
+            var res = new int[] {0, 0, 0, 0, 0, 0};
+
             int valA0 = rollD6();
             int valA1 = rollD6();
-            int del = Math.Abs(valA0 - valA1);
-            //0,1,2,3,4,5
-            var res = new int[6];
-            if (del==0)
+            int delA = Math.Abs(valA0 - valA1);
+            if (delA<=1)
             {
-                res[0] = valA0;
-                res[1] = valA1;
+                res[0] = valA0 >= valA1 ? valA0 : valA1;
+                res[3] = valA0 >= valA1 ? valA1 : valA0;
             }
-            else if (del<=1)
+            else if (delA <= 3)
             {
-                res[0] = valA0 > valA1 ? valA0 : valA1;
-                res[1] = valA0 < valA1 ? valA0 : valA1;
+                res[1] = valA0 >= valA1 ? valA0 : valA1;
+                res[4] = valA0 >= valA1 ? valA1 : valA0;
             }
-            else if (del >= 4)
+            else
             {
-                res[2] = valA0 < valA1 ? valA0 : valA1;
-                res[5] = valA0 > valA1 ? valA0 : valA1;
+                res[2] = valA0 >= valA1 ? valA0 : valA1;
+                res[5] = valA0 >= valA1 ? valA1 : valA0;
             }
 
-            return 0;
+            int valB0 = rollD6();
+            int valB1 = rollD6();
+            int delB = Math.Abs(valA0 - valA1);
+
+            if (delB<=1)
+            {
+                if (res[0]!=0)
+                {
+                    res[1] = valB0 >= valB1 ? valB1 : valB0;
+                    res[4] = valB0 >= valB1 ? valB0 : valB1;
+                }
+                else
+                {
+                    res[0] = valB0 >= valB1 ? valB0 : valB1;
+                    res[3] = valB0 >= valB1 ? valB1 : valB0;
+                }
+            }
+            else if (delA <= 3)
+            {
+                if (res[0] != 0)
+                {
+                    res[1] = valB0 >= valB1 ? valB0 : valB1;
+                    res[4] = valB0 >= valB1 ? valB1 : valB0;
+                }
+                else
+                {
+                    res[2] = valB0 >= valB1 ? valB0 : valB1;
+                    res[5] = valB0 >= valB1 ? valB1 : valB0;
+                }
+            }
+            else
+            {
+                if (res[2]!=0)
+                {
+                    res[1] = valB0 >= valB1 ? valB0 : valB1;
+                    res[4] = valB0 >= valB1 ? valB1 : valB0;
+                }
+                else
+                {
+                    res[2] = valB0 >= valB1 ? valB0 : valB1;
+                    res[5] = valB0 >= valB1 ? valB1 : valB0;
+                }
+            }
+            
+            int valC0 = rollD6();
+            int valC1 = rollD6();
+
+            int indexC0 = -1;
+            int indexC1 = -1;
+            
+            for (int i = 0; i < 6; i++)
+            {
+                if (res[i]==0)
+                {
+                    res[i] = valC0;
+                    indexC0 = i;
+                    break;
+                }
+            }
+            
+            for (int i = 0; i < 6; i++)
+            {
+                if (res[i]==0)
+                {
+                    res[i] = valC1;
+                    indexC1 = i;
+                    break;
+                }
+            }
+            
+            var resTC0 = res[0] * 100 + res[1] * 10 + res[2];
+            var resTC1 = res[3] * 100 + res[4] * 10 + res[5];
+            var resTCA=resTC0 - resTC1;
+
+            var tmp = res[indexC1];
+            res[indexC1] = res[indexC0];
+            res[indexC0] = tmp;
+            
+            var resTC2 = res[0] * 100 + res[1] * 10 + res[2];
+            var resTC3 = res[3] * 100 + res[4] * 10 + res[5];
+            var resTCB= resTC2 - resTC3;
+            
+            if (resTCA<0&&resTCB<0)
+            {
+                return resTCA;
+            }
+            if (resTCA < 0 || resTCB < 0)
+            {
+                return resTCA >= 0 ? resTCA : resTCB;
+            }
+            return resTCA <= resTCB ? resTCA : resTCB;
         }
 
+        bool CompareVal((int, int) A, (int, int) B)
+        {
+            return A.Item1 > B.Item1;
+        }
+
+        int doSearch_SemiTheoreticalBest()
+        {
+            var raw_res = new int[6];
+            for (var i = 0; i < raw_res.Length; i++)
+            {
+                raw_res[i] = rollD6();
+            }
+
+            var indexLib = new (int, int)[15];
+            indexLib[0] = (0, 1);
+            indexLib[1] = (0, 2);
+            indexLib[2] = (0, 3);
+            indexLib[3] = (0, 4);
+            indexLib[4] = (0, 5);
+            indexLib[5] = (1, 2);
+            indexLib[6] = (1, 3);
+            indexLib[7] = (1, 4);
+            indexLib[8] = (1, 5);
+            indexLib[9] = (2, 3);
+            indexLib[10] = (2, 4);
+            indexLib[11] = (2, 5);
+            indexLib[12] = (3, 4);
+            indexLib[13] = (3, 5);
+            indexLib[14] = (4, 5);
+
+            var dels = new (int, int)[15];
+            for (var i = 0; i < indexLib.Length; i++)
+            {
+                dels[i].Item1 = Math.Abs(raw_res[indexLib[i].Item1] - raw_res[indexLib[i].Item2]);
+                dels[i].Item2 = i;
+            }
+
+            var delOrdered = dels.OrderBy(v => v.Item1).ToArray();
+
+            (int, int) first = (0, 0);
+            (int, int) second = (0, 0);
+            (int, int) third = (0, 0);
+
+            first = indexLib[delOrdered[0].Item1];
+            int j = 0;
+            for (int i = 1; i < delOrdered.Length; i++)
+            {
+                var tmp = indexLib[delOrdered[i].Item1];
+                if (first.Item1 != tmp.Item1 && first.Item1 != tmp.Item2)
+                {
+                    if (first.Item2 != tmp.Item1 && first.Item2 != tmp.Item2)
+                    {
+                        second = tmp;
+                        j = 1;
+                    }
+                }
+            }
+
+            for (int i = j + 1; i < delOrdered.Length; i++)
+            {
+                var tmp = indexLib[delOrdered[i].Item1];
+                if (second.Item1 != tmp.Item1 && second.Item1 != tmp.Item2)
+                {
+                    if (second.Item2 != tmp.Item1 && second.Item2 != tmp.Item2)
+                    {
+                        third = tmp;
+                    }
+                }
+            }
+
+            var res = new int[6];
+            res[0] = raw_res[first.Item1] >= raw_res[first.Item2] ? raw_res[first.Item1] : raw_res[first.Item2];
+            res[3] = raw_res[first.Item1] >= raw_res[first.Item2] ? raw_res[first.Item2] : raw_res[first.Item1];
+            res[1] = raw_res[second.Item1] >= raw_res[second.Item2] ? raw_res[second.Item2] : raw_res[second.Item1];
+            res[4] = raw_res[second.Item1] >= raw_res[second.Item2] ? raw_res[second.Item1] : raw_res[second.Item2];
+            res[2] = raw_res[third.Item1] >= raw_res[third.Item2] ? raw_res[third.Item2] : raw_res[third.Item1];
+            res[5] = raw_res[third.Item1] >= raw_res[third.Item2] ? raw_res[third.Item1] : raw_res[third.Item2];
+
+            var resT1 = res[0] * 100 + res[1] * 10 + res[2];
+            var resT2 = res[3] * 100 + res[4] * 10 + res[5];
+            return resT1 - resT2;
+        }
+
+        [Button]
+        void SemiTheoreticalBest()
+        {
+            int Trial = 1000000;
+            int badCount = 0;
+            int critCount = 0;
+            int goodCount = 0;
+            int fairCount = 0;
+            for (int i = 0; i < Trial; i++)
+            {
+                int res = doSearch_SemiTheoreticalBest();
+                if (res < 0||res>=100)
+                {
+                    badCount++;
+                }
+                else if (res == 0)
+                {
+                    critCount++;
+                }
+                else if(res<=10)
+                {
+                    goodCount++;
+                }
+                else
+                {
+                    fairCount++;
+                }
+            }
+
+            float badPer = (badCount / (float) Trial) * 100;
+            float critPer = (critCount / (float) Trial) * 100;
+            float goodPer = (goodCount / (float) Trial) * 100;
+            float fairPer = (fairCount / (float) Trial) * 100;
+            Debug.Log("Using Random Method Searching--bad:" + badPer + "% crit:" + critPer +
+                      "% goodPer:" + goodPer + "% fairPer:" + fairPer+"%");
+        }
+        
         [Button]
         void PureRandomSearch()
         {
@@ -167,8 +379,7 @@ namespace ROOT
             int badCount = 0;
             int critCount = 0;
             int goodCount = 0;
-            int goodSum = 0;
-            float goodAvg = 0.0f;
+            int fairCount = 0;
             for (int i = 0; i < Trial; i++)
             {
                 int res = doSearch_pureRandom();
@@ -180,20 +391,22 @@ namespace ROOT
                 {
                     critCount++;
                 }
-                else
+                else if(res<=10)
                 {
                     goodCount++;
-                    goodSum += res;
                 }
-
-                goodAvg = goodSum / (float) goodCount;
+                else
+                {
+                    fairCount++;
+                }
             }
 
             float badPer = (badCount / (float) Trial) * 100;
             float critPer = (critCount / (float) Trial) * 100;
             float goodPer = (goodCount / (float) Trial) * 100;
-            Debug.Log("Using Random Method Searching--bad:" + badPer + "% crit:" + critPer + "% good:" + goodPer +
-                      "%@" + goodAvg);
+            float fairPer = (fairCount / (float) Trial) * 100;
+            Debug.Log("Using Random Method Searching--bad:" + badPer + "% crit:" + critPer +
+                      "% goodPer:" + goodPer + "% fairPer:" + fairPer+"%");
         }
 
         [Button]
@@ -203,8 +416,7 @@ namespace ROOT
             int badCount = 0;
             int critCount = 0;
             int goodCount = 0;
-            int goodSum = 0;
-            float goodAvg = 0.0f;
+            int fairCount = 0;
             for (int i = 0; i < Trial; i++)
             {
                 int res = doSearch_BasicTechniqueA();
@@ -216,22 +428,61 @@ namespace ROOT
                 {
                     critCount++;
                 }
-                else
+                else if(res<=10)
                 {
                     goodCount++;
-                    goodSum += res;
                 }
-
-                goodAvg = goodSum / (float) goodCount;
+                else
+                {
+                    fairCount++;
+                }
             }
 
             float badPer = (badCount / (float) Trial) * 100;
             float critPer = (critCount / (float) Trial) * 100;
             float goodPer = (goodCount / (float) Trial) * 100;
-            Debug.Log("Using Basic Technique A Method Searching--bad:" + badPer + "% crit:" + critPer + "% good:" + goodPer +
-                      "%@" + goodAvg);
-        }*/
+            float fairPer = (fairCount / (float) Trial) * 100;
+            Debug.Log("Using Random Method Searching--bad:" + badPer + "% crit:" + critPer +
+                      "% goodPer:" + goodPer + "% fairPer:" + fairPer+"%");
+        }
 
+        [Button]
+        void BasicTechniqueB()
+        {
+            int Trial = 1000000;
+            int badCount = 0;
+            int critCount = 0;
+            int goodCount = 0;
+            int fairCount = 0;
+            for (int i = 0; i < Trial; i++)
+            {
+                int res = doSearch_BasicTechniqueB();
+                if (res < 0||res>=100)
+                {
+                    badCount++;
+                }
+                else if (res == 0)
+                {
+                    critCount++;
+                }
+                else if(res<=10)
+                {
+                    goodCount++;
+                }
+                else
+                {
+                    fairCount++;
+                }
+            }
+
+            float badPer = (badCount / (float) Trial) * 100;
+            float critPer = (critCount / (float) Trial) * 100;
+            float goodPer = (goodCount / (float) Trial) * 100;
+            float fairPer = (fairCount / (float) Trial) * 100;
+            Debug.Log("Using Random Method Searching--bad:" + badPer + "% crit:" + critPer +
+                      "% goodPer:" + goodPer + "% fairPer:" + fairPer+"%");
+        }*/
+        
         void Awake()
         {
             //这里不能用Time.time，因为Awake和游戏运行时间差距一般很小且固定。所以这里要去调系统时间
