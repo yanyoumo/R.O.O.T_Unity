6eefc511ce4b0e8133589d80d2569a908c641f18
syx007 <suyuxuan007@hotmail.com>
2021-02-02 21:15:52 +0800

    change CareerSetupScene UI element to all Doozy UI element

--- ROOT_demo/Assets/Script/CareerSetupManger.cs
+++ ROOT_demo/Assets/Script/CareerSetupManger.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using Doozy.Engine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -24,16 +23,12 @@ namespace ROOT
     public class CareerSetupManger : MonoBehaviour
     {
         //public LevelActionAsset[] ClassicGameActionAssetLib => LevelLib.Instance.CareerActionAssetList;
-        public Button BackButton;
-        public Button ContinueButton;
         public static int sceneId;
 
         // Start is called before the first frame update
         void Awake()
         {
-            //这种“自我初始化”建议卸载Awake里面。
-            BackButton.onClick.AddListener(Back);
-            ContinueButton.onClick.AddListener(Continue);
+
         }
 
         private void OnGUI()
@@ -41,16 +36,17 @@ namespace ROOT
             //这里的函数到时候写在这里、这个和Update等效的；只不过是跟UI的更新更加相关。
         }
 
-        void Back()
+        public void Back()
         {
             SceneManager.LoadScene(StaticName.SCENE_ID_CAREER, LoadSceneMode.Additive);
             SceneManager.UnloadSceneAsync(StaticName.SCENE_ID_CAREERSETUP);
         }
 
-        void Continue()
+        public void Continue()
         {
-            //var additionalGameSetup = new AdditionalGameSetup();//这个就是需要这个界面去设置、并且注入的“数据包”。
+            var additionalGameSetup = new AdditionalGameSetup();//这个就是需要这个界面去设置、并且注入的“数据包”。
             var actionAsset = LevelLib.Instance.CareerActionAssetList[sceneId];
+            actionAsset.AdditionalGameSetup = additionalGameSetup;
             LevelMasterManager.Instance.LoadLevelThenPlay(actionAsset);
             SceneManager.UnloadSceneAsync(StaticName.SCENE_ID_CAREERSETUP);
         }
