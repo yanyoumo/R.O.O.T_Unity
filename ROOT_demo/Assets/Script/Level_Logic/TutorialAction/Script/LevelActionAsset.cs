f76adee223345de9d93a3fd8065353499960f2f8
yanyoumo <yanyoumo@gmail.com>
2021-02-24 11:08:03 +0800

    round部分整理完毕

--- ROOT_demo/Assets/Script/Level_Logic/TutorialAction/Script/LevelActionAsset.cs
+++ ROOT_demo/Assets/Script/Level_Logic/TutorialAction/Script/LevelActionAsset.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Sirenix.OdinInspector;
@@ -9,47 +8,9 @@ using UnityEngine;
 namespace ROOT.SetupAsset
 {
     [Serializable]
-    public struct TutorialActionData
-    {
-        [HorizontalGroup("Split")] [VerticalGroup("Split/Left")] [LabelWidth(80)]
-        public int ActionIdx;
-
-        [EnumPaging] [VerticalGroup("Split/Right")]
-        public TutorialActionType ActionType;
-
-        [ShowIf("ActionType", TutorialActionType.Text)] [LabelWidth(30)]
-        public string Text;
-
-        [ShowIf("ActionType", TutorialActionType.Text)] [HorizontalGroup("Doppelganger")] [LabelWidth(135)]
-        public bool DoppelgangerToggle;
-
-        [ShowIf("@this.ActionType==TutorialActionType.Text&&this.DoppelgangerToggle")]
-        [HorizontalGroup("Doppelganger")]
-        [LabelWidth(135)]
-        public string DoppelgangerText;
-
-        [ShowIf("@this.ActionType==TutorialActionType.CreateUnit||this.ActionType==TutorialActionType.CreateCursor")]
-        public Vector2Int Pos;
-
-        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [VerticalGroup("Split/Left")]
-        public SignalType Core;
-
-        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [VerticalGroup("Split/Left")]
-        public HardwareType HardwareType;
-
-        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [VerticalGroup("Split/Right")]
-        public SideType[] Sides;
-
-        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [Range(1, 5)]
-        public int Tier;
-    }
-
-    [Serializable]
     [CreateAssetMenu(fileName = "NewActionAsset", menuName = "ActionAsset/New ActionAsset")]
     public class LevelActionAsset : SerializedScriptableObject
     {
-        [HideInInspector] public BossAssetLib BossLib;
-
         [Header("Basic Data")] public string TitleTerm;
 
         [AssetSelector(Filter = "t:Sprite", Paths = "Assets/Resources/UIThumbnail/TutorialThumbnail")]
@@ -69,40 +30,15 @@ namespace ROOT.SetupAsset
         [ShowIf("levelType", LevelType.Career)]
         public UnitGist[] InitalBoard;
 
-        [ShowIf("levelType", LevelType.Career)]
-        public int NormalRoundCount;
-
-        [ShowIf("levelType", LevelType.Career)]
+        [ShowIf("levelType", LevelType.Career)] [OnValueChanged("HasBossChanged")]
         public bool HasBossRound;
 
         [ShowIf("levelType", LevelType.Career)] [HideIf("HasBossRound")]
         public bool Endless;
 
         [ShowIf("levelType", LevelType.Career)] [ShowIf("HasBossRound")]
+        [OnValueChanged("BossTypeChanged")]
         public BossStageType BossStage;
-        
-        [ShowIf("levelType", LevelType.Career)]
-        [Button("Create New RoundDatas")]
-        public void CreateRoundDatasFromGist()
-        {
-            if (NormalRoundCount <= 0)
-            {
-                Debug.LogError("Can't create zero length Rounds");
-            }
-            else
-            {
-                RoundLibVal = new RoundLib();
-                for (int i = 0; i < NormalRoundCount; i++)
-                {
-                    RoundLibVal.Add(new RoundData {ID = i});
-                }
-
-                if (HasBossRound)
-                {
-                    BossSetup = new BossAdditionalSetupAsset {BossStageTypeVal = BossStage};
-                }
-            }
-        }
 
         [OdinSerialize] [ShowIf("levelType", LevelType.Career)]
         public RoundLib RoundLibVal;
@@ -131,31 +67,25 @@ namespace ROOT.SetupAsset
             {
                 if (HasBossRound && Endless)
                 {
+#if UNITY_EDITOR
+                    return false;
+#else
                     throw new Exception("a round lib couldn't has boss and being endless");
+#endif
                 }
-
                 return Endless;
             }
         }
 
+        [ShowInInspector]
         public int PlayableCount
         {
             get
             {
-                if (GetEndless)
-                    return int.MaxValue;
-                else
-                {
-                    if (!HasBossRound)
-                    {
-                        return RoundLibVal.Sum(round => round.TotalLength);
-                    }
-                    else
-                    {
-                        Debug.Log("BossSetup.BossLength:" + BossSetup.BossLength);
-                        return RoundLibVal.Sum(round => round.TotalLength) + BossSetup.BossLength;
-                    }
-                }
+                if (GetEndless) return int.MaxValue;
+                if (RoundLibVal == null) return 0;
+                if (!HasBossRound) return RoundLibVal.Sum(round => round.TotalLength);
+                return RoundLibVal.Sum(round => round.TotalLength) + BossSetup.BossLength;
             }
         }
 
@@ -170,11 +100,24 @@ namespace ROOT.SetupAsset
         
         [ShowInInspector] [ShowIf("HasBossRound")]
         public BossAdditionalSetupAsset BossSetup;
+
+        private void HasBossChanged()
+        {
+            if (HasBossRound)
+            {
+                Endless = false;
+            }
+        }
+        
+        private void BossTypeChanged()
+        {
+            BossSetup.BossStageTypeVal = BossStageVal;
+        }
         
         [Obsolete("Why?")] public Vector2Int[] StationaryRateList => null;
 
         [Obsolete("Why?")] public List<SignalType> ShopExcludedType => null;
 
-        [Obsolete("Why?")] public bool ExcludedShop = false;
+        [Obsolete("Why?")] [HideInInspector] public bool ExcludedShop = false;
     }
 }
---
+++ ROOT_demo/Assets/Script/Level_Logic/TutorialAction/Script/TutorialActionData.cs
@@ -0,0 +1,42 @@
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ROOT.SetupAsset
+{
+    [Serializable]
+    public struct TutorialActionData
+    {
+        [HorizontalGroup("Split")] [VerticalGroup("Split/Left")] [LabelWidth(80)]
+        public int ActionIdx;
+
+        [EnumPaging] [VerticalGroup("Split/Right")]
+        public TutorialActionType ActionType;
+
+        [ShowIf("ActionType", TutorialActionType.Text)] [LabelWidth(30)]
+        public string Text;
+
+        [ShowIf("ActionType", TutorialActionType.Text)] [HorizontalGroup("Doppelganger")] [LabelWidth(135)]
+        public bool DoppelgangerToggle;
+
+        [ShowIf("@this.ActionType==TutorialActionType.Text&&this.DoppelgangerToggle")]
+        [HorizontalGroup("Doppelganger")]
+        [LabelWidth(135)]
+        public string DoppelgangerText;
+
+        [ShowIf("@this.ActionType==TutorialActionType.CreateUnit||this.ActionType==TutorialActionType.CreateCursor")]
+        public Vector2Int Pos;
+
+        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [VerticalGroup("Split/Left")]
+        public SignalType Core;
+
+        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [VerticalGroup("Split/Left")]
+        public HardwareType HardwareType;
+
+        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [VerticalGroup("Split/Right")]
+        public SideType[] Sides;
+
+        [ShowIf("ActionType", TutorialActionType.CreateUnit)] [Range(1, 5)]
+        public int Tier;
+    }
+}
