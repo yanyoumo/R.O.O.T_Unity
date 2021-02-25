724ba975375c539245190d6f9a6473dbd88c82dc
yanyoumo <yanyoumo@gmail.com>
2021-02-25 20:15:14 +0800

    Update UnitSignalCoreBase.cs

--- ROOT_demo/Assets/Resources/Signal/Script/Bases/UnitSignalCoreBase.cs
+++ ROOT_demo/Assets/Resources/Signal/Script/Bases/UnitSignalCoreBase.cs
@@ -66,7 +66,7 @@ namespace ROOT.Signal
         }
         
         //TODO 这里的流程要用新的SignalDataPackList系统，以前那些什么的SignalDepth不要用了。
-        public bool IsEndingScanFieldUnit => InServerGrid && (Owner.UnitSignal == SignalType.Scan && Owner.UnitHardware == HardwareType.Field) && ScanSignalPathDepth == 1;
+        public bool IsEndingScanFieldUnit => Owner.SignalCore is ScanUnitSignalCore {IsUnitVeryActive: true};
 
 
         //标记扫描信号的路径的参数。
