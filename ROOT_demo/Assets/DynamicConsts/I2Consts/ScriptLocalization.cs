a4b16482f75efd6131a2d0baa1b781f7cc7ac57e
yanyoumo <yanyoumo@gmail.com>
2021-03-31 10:45:54 +0800

    把两个经常重新编译的Const独立封装成dll

---
+++ ROOT_demo/Assets/DynamicConsts/I2Consts/ScriptLocalization.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+
+namespace I2.Loc
+{
+	public static class ScriptLocalization
+	{
+
+		public static string AdvShop_2_KM 		{ get{ return LocalizationManager.GetTranslation ("AdvShop_2_KM"); } }
+		public static string AdvShop_2_Touch 		{ get{ return LocalizationManager.GetTranslation ("AdvShop_2_Touch"); } }
+		public static string AdvShop_KM 		{ get{ return LocalizationManager.GetTranslation ("AdvShop_KM"); } }
+		public static string AdvShop_Touch 		{ get{ return LocalizationManager.GetTranslation ("AdvShop_Touch"); } }
+		public static string Back 		{ get{ return LocalizationManager.GetTranslation ("Back"); } }
+		public static string BasicControl_KM 		{ get{ return LocalizationManager.GetTranslation ("BasicControl_KM"); } }
+		public static string BasicControl_Touch 		{ get{ return LocalizationManager.GetTranslation ("BasicControl_Touch"); } }
+		public static string CareerButton 		{ get{ return LocalizationManager.GetTranslation ("CareerButton"); } }
+		public static string Continue 		{ get{ return LocalizationManager.GetTranslation ("Continue"); } }
+		public static string CoreSelection 		{ get{ return LocalizationManager.GetTranslation ("CoreSelection"); } }
+		public static string EndingMessageAcquiring_Failed 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageAcquiring_Failed"); } }
+		public static string EndingMessageAcquiring_Successed 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageAcquiring_Successed"); } }
+		public static string EndingMessageNoBoss_EarnedMoney 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageNoBoss_EarnedMoney"); } }
+		public static string EndingMessageNoBoss_NoEarnedMoney 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageNoBoss_NoEarnedMoney"); } }
+		public static string EndingMessageTelemetry_Failed 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageTelemetry_Failed"); } }
+		public static string EndingMessageTelemetry_Successed 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageTelemetry_Successed"); } }
+		public static string EndingMessageTutorial 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageTutorial"); } }
+		public static string EndingMessageTutorialFailed 		{ get{ return LocalizationManager.GetTranslation ("EndingMessageTutorialFailed"); } }
+		public static string GameCareerSetup 		{ get{ return LocalizationManager.GetTranslation ("GameCareerSetup"); } }
+		public static string GameOver 		{ get{ return LocalizationManager.GetTranslation ("GameOver"); } }
+		public static string HForHint_KM 		{ get{ return LocalizationManager.GetTranslation ("HForHint_KM"); } }
+		public static string HForHint_Touch 		{ get{ return LocalizationManager.GetTranslation ("HForHint_Touch"); } }
+		public static string KMEnterToComplete 		{ get{ return LocalizationManager.GetTranslation ("KMEnterToComplete"); } }
+		public static string KMEnterToReturn 		{ get{ return LocalizationManager.GetTranslation ("KMEnterToReturn"); } }
+		public static string KMHintCollapsed 		{ get{ return LocalizationManager.GetTranslation ("KMHintCollapsed"); } }
+		public static string KMHintExtend 		{ get{ return LocalizationManager.GetTranslation ("KMHintExtend"); } }
+		public static string Level_1 		{ get{ return LocalizationManager.GetTranslation ("Level_1"); } }
+		public static string Level_2 		{ get{ return LocalizationManager.GetTranslation ("Level_2"); } }
+		public static string Level_3 		{ get{ return LocalizationManager.GetTranslation ("Level_3"); } }
+		public static string Level_4 		{ get{ return LocalizationManager.GetTranslation ("Level_4"); } }
+		public static string Level_5 		{ get{ return LocalizationManager.GetTranslation ("Level_5"); } }
+		public static string Level_FSM__Acquiring_Test 		{ get{ return LocalizationManager.GetTranslation ("Level_FSM _Acquiring_Test"); } }
+		public static string Level_FSM__Telemetry_Test 		{ get{ return LocalizationManager.GetTranslation ("Level_FSM _Telemetry_Test"); } }
+		public static string Level_FSM_Barebone 		{ get{ return LocalizationManager.GetTranslation ("Level_FSM_Barebone"); } }
+		public static string Level_FSM_Career 		{ get{ return LocalizationManager.GetTranslation ("Level_FSM_Career"); } }
+		public static string Level_FSM_Test 		{ get{ return LocalizationManager.GetTranslation ("Level_FSM_Test"); } }
+		public static string Locked 		{ get{ return LocalizationManager.GetTranslation ("Locked"); } }
+		public static string Matrix_Core 		{ get{ return LocalizationManager.GetTranslation ("Matrix Core"); } }
+		public static string NextTutorial 		{ get{ return LocalizationManager.GetTranslation ("NextTutorial"); } }
+		public static string NextTutorialFailed 		{ get{ return LocalizationManager.GetTranslation ("NextTutorialFailed"); } }
+		public static string Notice 		{ get{ return LocalizationManager.GetTranslation ("Notice"); } }
+		public static string OK 		{ get{ return LocalizationManager.GetTranslation ("OK"); } }
+		public static string PlayLevel 		{ get{ return LocalizationManager.GetTranslation ("PlayLevel"); } }
+		public static string ReleaseToReturn_KM 		{ get{ return LocalizationManager.GetTranslation ("ReleaseToReturn_KM"); } }
+		public static string Restart 		{ get{ return LocalizationManager.GetTranslation ("Restart"); } }
+		public static string Scan_Core 		{ get{ return LocalizationManager.GetTranslation ("Scan Core"); } }
+		public static string SelectTwoCore 		{ get{ return LocalizationManager.GetTranslation ("SelectTwoCore"); } }
+		public static string StartGame 		{ get{ return LocalizationManager.GetTranslation ("StartGame"); } }
+		public static string Thermal_Core 		{ get{ return LocalizationManager.GetTranslation ("Thermal Core"); } }
+		public static string TouchHintCollapsed 		{ get{ return LocalizationManager.GetTranslation ("TouchHintCollapsed"); } }
+		public static string TouchHintExtend 		{ get{ return LocalizationManager.GetTranslation ("TouchHintExtend"); } }
+		public static string TouchToComplete 		{ get{ return LocalizationManager.GetTranslation ("TouchToComplete"); } }
+		public static string TouchToReturn 		{ get{ return LocalizationManager.GetTranslation ("TouchToReturn"); } }
+		public static string Tutorial 		{ get{ return LocalizationManager.GetTranslation ("Tutorial"); } }
+		public static string TutorialBasicControl 		{ get{ return LocalizationManager.GetTranslation ("TutorialBasicControl"); } }
+		public static string TutorialComplexSignal 		{ get{ return LocalizationManager.GetTranslation ("TutorialComplexSignal"); } }
+		public static string TutorialDestroyer 		{ get{ return LocalizationManager.GetTranslation ("TutorialDestroyer"); } }
+		public static string TutorialGoal 		{ get{ return LocalizationManager.GetTranslation ("TutorialGoal"); } }
+		public static string TutorialGoalAndCycle 		{ get{ return LocalizationManager.GetTranslation ("TutorialGoalAndCycle"); } }
+		public static string TutorialSectionOver 		{ get{ return LocalizationManager.GetTranslation ("TutorialSectionOver"); } }
+		public static string TutorialShop 		{ get{ return LocalizationManager.GetTranslation ("TutorialShop"); } }
+		public static string TutorialShop_Currency 		{ get{ return LocalizationManager.GetTranslation ("TutorialShop_Currency"); } }
+		public static string TutorialSignalBasic 		{ get{ return LocalizationManager.GetTranslation ("TutorialSignalBasic"); } }
+		public static string TutorialSignalBasic_Adv 		{ get{ return LocalizationManager.GetTranslation ("TutorialSignalBasic_Adv"); } }
+		public static string TutorialTimeAdv 		{ get{ return LocalizationManager.GetTranslation ("TutorialTimeAdv"); } }
+	}
+
+    public static class ScriptTerms
+	{
+
+		public const string AdvShop_2_KM = "AdvShop_2_KM";
+		public const string AdvShop_2_Touch = "AdvShop_2_Touch";
+		public const string AdvShop_KM = "AdvShop_KM";
+		public const string AdvShop_Touch = "AdvShop_Touch";
+		public const string Back = "Back";
+		public const string BasicControl_KM = "BasicControl_KM";
+		public const string BasicControl_Touch = "BasicControl_Touch";
+		public const string CareerButton = "CareerButton";
+		public const string Continue = "Continue";
+		public const string CoreSelection = "CoreSelection";
+		public const string EndingMessageAcquiring_Failed = "EndingMessageAcquiring_Failed";
+		public const string EndingMessageAcquiring_Successed = "EndingMessageAcquiring_Successed";
+		public const string EndingMessageNoBoss_EarnedMoney = "EndingMessageNoBoss_EarnedMoney";
+		public const string EndingMessageNoBoss_NoEarnedMoney = "EndingMessageNoBoss_NoEarnedMoney";
+		public const string EndingMessageTelemetry_Failed = "EndingMessageTelemetry_Failed";
+		public const string EndingMessageTelemetry_Successed = "EndingMessageTelemetry_Successed";
+		public const string EndingMessageTutorial = "EndingMessageTutorial";
+		public const string EndingMessageTutorialFailed = "EndingMessageTutorialFailed";
+		public const string GameCareerSetup = "GameCareerSetup";
+		public const string GameOver = "GameOver";
+		public const string HForHint_KM = "HForHint_KM";
+		public const string HForHint_Touch = "HForHint_Touch";
+		public const string KMEnterToComplete = "KMEnterToComplete";
+		public const string KMEnterToReturn = "KMEnterToReturn";
+		public const string KMHintCollapsed = "KMHintCollapsed";
+		public const string KMHintExtend = "KMHintExtend";
+		public const string Level_1 = "Level_1";
+		public const string Level_2 = "Level_2";
+		public const string Level_3 = "Level_3";
+		public const string Level_4 = "Level_4";
+		public const string Level_5 = "Level_5";
+		public const string Level_FSM__Acquiring_Test = "Level_FSM _Acquiring_Test";
+		public const string Level_FSM__Telemetry_Test = "Level_FSM _Telemetry_Test";
+		public const string Level_FSM_Barebone = "Level_FSM_Barebone";
+		public const string Level_FSM_Career = "Level_FSM_Career";
+		public const string Level_FSM_Test = "Level_FSM_Test";
+		public const string Locked = "Locked";
+		public const string Matrix_Core = "Matrix Core";
+		public const string NextTutorial = "NextTutorial";
+		public const string NextTutorialFailed = "NextTutorialFailed";
+		public const string Notice = "Notice";
+		public const string OK = "OK";
+		public const string PlayLevel = "PlayLevel";
+		public const string ReleaseToReturn_KM = "ReleaseToReturn_KM";
+		public const string Restart = "Restart";
+		public const string Scan_Core = "Scan Core";
+		public const string SelectTwoCore = "SelectTwoCore";
+		public const string StartGame = "StartGame";
+		public const string Thermal_Core = "Thermal Core";
+		public const string TouchHintCollapsed = "TouchHintCollapsed";
+		public const string TouchHintExtend = "TouchHintExtend";
+		public const string TouchToComplete = "TouchToComplete";
+		public const string TouchToReturn = "TouchToReturn";
+		public const string Tutorial = "Tutorial";
+		public const string TutorialBasicControl = "TutorialBasicControl";
+		public const string TutorialComplexSignal = "TutorialComplexSignal";
+		public const string TutorialDestroyer = "TutorialDestroyer";
+		public const string TutorialGoal = "TutorialGoal";
+		public const string TutorialGoalAndCycle = "TutorialGoalAndCycle";
+		public const string TutorialSectionOver = "TutorialSectionOver";
+		public const string TutorialShop = "TutorialShop";
+		public const string TutorialShop_Currency = "TutorialShop_Currency";
+		public const string TutorialSignalBasic = "TutorialSignalBasic";
+		public const string TutorialSignalBasic_Adv = "TutorialSignalBasic_Adv";
+		public const string TutorialTimeAdv = "TutorialTimeAdv";
+	}
+}
