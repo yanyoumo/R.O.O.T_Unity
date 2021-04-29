dbf2bcfd62864016601d4ebd5b797e57f738db13
Digong Jiang <jiangdigong@gmail.com>
2021-04-29 20:49:25 +0800

    update func, action 52

--- ROOT_demo/Assets/Script/FSM/TutorialCheckFunctionList.cs
+++ ROOT_demo/Assets/Script/FSM/TutorialCheckFunctionList.cs
@@ -122,7 +122,7 @@ namespace ROOT
             var unit = board.FindUnitByPos(new Vector2Int(2,1));
             if (unit == null)
                 return false;
-            return unit.UnitSignal == SignalType.Matrix && board.CheckAllActive();
+            return unit.UnitSignal == SignalType.Matrix && board.GetUnitsConnectedIsland() == 1;
         }
 
         public static bool Buy3UnitsOrNotEnoughMoney(FSMLevelLogic fsm, Board board)
