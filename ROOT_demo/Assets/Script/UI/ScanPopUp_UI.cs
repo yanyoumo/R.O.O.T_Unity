1c103f25d8f4552e81187b85751b1c2bb29d10d2
yanyoumo <yanyoumo@gmail.com>
2021-06-08 12:01:57 +0800

    扫描解锁UI逻辑微调优化

--- ROOT_demo/Assets/Script/UI/ScanPopUp_UI.cs
+++ ROOT_demo/Assets/Script/UI/ScanPopUp_UI.cs
@@ -1,6 +1,7 @@
 using System;
 using com.ootii.Messages;
 using DG.Tweening;
+using Doozy.Engine.UI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,10 +10,13 @@ namespace ROOT.UI
 {
     public class ScanPopUp_UI : MonoBehaviour
     {
+        public UIView PopUpUI;
+        public UIToggle ScanToggle;
         public TextMeshProUGUI PleaseReadText;
         public TextMeshProUGUI OkText;
         public Button ProceeButton;
-        private const float totalTime = 5;
+        private const float totalTime = 5.0f;
+        private const string DOTweenTimerID = "ScanPopUp_UI_TimerDOTween";
 
         private void SetCounterAndText(float x)
         {
@@ -38,11 +42,20 @@ namespace ROOT.UI
             PlayerPrefs.SetInt(Consts.StaticPlayerPrefName.UNLOCK_SCAN, 1);
             PlayerPrefs.Save();
             MessageDispatcher.SendMessage(WorldEvent.ScanUnitLockChangedEvent);
+            PopUpUI.Hide();
+        }
+
+        public void PopUINotButtonPressed()
+        {
+            ScanToggle.IsOn = false;
+            PopUpUI.Hide();
         }
         
         private void OnEnable()
         {
-            DOTween.To(() => totalTime, SetCounterAndText, 0.0f, totalTime).SetEase(Ease.Linear).onComplete = AllOkButton;
+            DOTween.To(() => totalTime, SetCounterAndText, 0.0f, totalTime)
+                .SetId(DOTweenTimerID)
+                .SetEase(Ease.Linear).onComplete = AllOkButton;
         }
         
         private void OnDisable()
@@ -50,6 +63,7 @@ namespace ROOT.UI
             PleaseReadText.enabled = true;
             OkText.enabled = false;
             ProceeButton.interactable = false;
+            DOTween.Kill(DOTweenTimerID);
         }
     }
 }
