baa67c4ecbd022b7c050d6e8478d8ecf4021dee1
Yan Youmo <1737612133@qq.com>
2021-06-09 20:55:41 +0800

    添加LevelFeature相关的Mgr代码和Shader、材质

---
+++ ROOT_demo/Assets/Script/UI/LevelFeatureIconArray_UI.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ROOT.UI
+{
+    public class LevelFeatureIconArray_UI : MonoBehaviour
+    {
+        public Material DefaultMat;
+        public Material BWMat;
+        
+        public Sprite[] IconSprites;
+        public Image[] IconImages;
+        
+        public void SetLevelFeature(LevelFeature _levelFeature)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                var currentFlag = 1 << i;
+                var hasFlag = ((int) _levelFeature & currentFlag) == currentFlag;
+                IconImages[i].material = hasFlag ? DefaultMat : BWMat;
+            }
+        }
+        
+        private void Awake()
+        {
+            Debug.Assert(IconSprites.Length == IconImages.Length, "Target Image and Sprites count not matching.");
+            for (var i = 0; i < IconImages.Length; i++)
+            {
+                IconImages[i].sprite = IconSprites[i];
+            }
+        }
+    }
+}
