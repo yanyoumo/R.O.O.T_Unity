0baa0deb911b3c5c726720648a2260d226e34165
yanyoumo <yanyoumo@gmail.com>
2021-06-21 21:19:14 +0800

    更新防火墙数据流程

--- ROOT_demo/Assets/Script/Backbone/Signal/SignalAssets/FirewallSignalAsset.cs
+++ ROOT_demo/Assets/Script/Backbone/Signal/SignalAssets/FirewallSignalAsset.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ROOT.Signal
 {
+    using FirewallCircle=List<Vector2Int>;
     public class FirewallSignalAsset : SignalAssetBase
     {
         public override Type UnitSignalCoreType => typeof(FirewallUnitSignalCore);
         public override SignalType SignalType => SignalType.Firewall;
+
+        private List<FirewallCircle> getFireWallCircleLists(Board _gameBoard)
+        {
+            return new List<FirewallCircle>();
+        }
     }
 }
