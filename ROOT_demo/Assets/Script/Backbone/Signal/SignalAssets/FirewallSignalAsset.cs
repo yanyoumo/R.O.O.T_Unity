19ad671635023e454a6337738a144cc1477741d7
yanyoumo <yanyoumo@gmail.com>
2021-06-24 20:41:13 +0800

    Update FirewallSignalAsset.cs

--- ROOT_demo/Assets/Script/Backbone/Signal/SignalAssets/FirewallSignalAsset.cs
+++ ROOT_demo/Assets/Script/Backbone/Signal/SignalAssets/FirewallSignalAsset.cs
@@ -31,8 +31,15 @@ namespace ROOT.Signal
         private void BoardDataUpdatedHandler(IMessage rMessage)
         {
             var data = new Unit[0];
-            SignalMasterMgr.Instance.Paths[SignalType.Firewall].ForEach(u => data.AddRange(
-                u.Where(u0=>u0.UnitHardware==HardwareType.Field)));
+            try
+            {
+                SignalMasterMgr.Instance.Paths[SignalType.Firewall].ForEach(u => data.AddRange(u.Where(u0=>u0.UnitHardware==HardwareType.Field)));
+            }
+            catch (KeyNotFoundException)
+            {
+                return;
+            }
+
             if (data.Length != 0)
             {
                 updateFireWallCircle(data);
