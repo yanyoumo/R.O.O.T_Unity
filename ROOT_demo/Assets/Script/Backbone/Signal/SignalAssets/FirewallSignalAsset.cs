185e55c30d59056d111c214d4607372613a0ac94
Digong Jiang <jiangdigong@gmail.com>
2021-06-29 20:03:50 +0800

    DeleteCutVertex

--- ROOT_demo/Assets/Script/Backbone/Signal/SignalAssets/FirewallSignalAsset.cs
+++ ROOT_demo/Assets/Script/Backbone/Signal/SignalAssets/FirewallSignalAsset.cs
@@ -24,8 +24,13 @@ namespace ROOT.Signal
         private List<FirewallCircle> _connectComponent;
         private static int[] dx4 = { 0, -1, 0, 1 };
         private static int[] dy4 = { -1, 0, 1, 0 };
-        private static int[] dx8 = {0, -1, -1, -1, 0, 1, 1, 1};
-        private static int[] dy8 = {-1, -1, 0, 1, 1, 1, 0, -1};
+        private static int[] dx8 = { 0, -1, -1, -1, 0, 1, 1, 1 };
+        private static int[] dy8 = { -1, -1, 0, 1, 1, 1, 0, -1 };
+        private Dictionary<Vector2Int, List<Vector2Int>> edge;
+        private Dictionary<Vector2Int, int> low;
+        private HashSet<Vector2Int> cutVertexSet;
+        private Dictionary<Vector2Int, int> dfn;
+        private int dfsClock;
         private void bfs(int x, int y)
         {
             var queue = new Queue<Vector2Int>();
@@ -37,11 +42,10 @@ namespace ROOT.Signal
                 for (var i = 0; i < 4; ++i)
                 {
                     int xx = now.x + dx4[i], yy = now.y + dy4[i];
-                    if (xx >= 0 && xx < N && yy >= 0 && yy < N && _board[xx, yy] == 0)
-                    {
-                        _board[xx, yy] = -1;
-                        queue.Enqueue(new Vector2Int(xx, yy));
-                    }
+                    if (xx < 0 || xx >= N || yy < 0 || yy >= N || _board[xx, yy] != 0)
+                        continue;
+                    _board[xx, yy] = -1;
+                    queue.Enqueue(new Vector2Int(xx, yy));
                 }
             }
         }
@@ -52,19 +56,18 @@ namespace ROOT.Signal
             var queue = new Queue<Vector2Int>();
             res.Add(new Vector2Int(x, y));
             queue.Enqueue(new Vector2Int(x, y));
-            _board[x, y] = -1;
+            _board[x, y] = 2;
             while (queue.Count != 0)
             {
                 var now = queue.Dequeue();
                 for (var i = 0; i < 4; ++i)
                 {
                     int xx = now.x + dx4[i], yy = now.y + dy4[i];
-                    if (xx >= 0 && xx < N && yy >= 0 && yy < N && _board[xx, yy] != -1)
-                    {
-                        _board[xx, yy] = 2;
-                        res.Add(new Vector2Int(xx, yy));
-                        queue.Enqueue(new Vector2Int(xx, yy));
-                    }
+                    if (xx < 0 || xx >= N || yy < 0 || yy >= N || _board[xx, yy] == -1)
+                        continue;
+                    _board[xx, yy] = 2;
+                    res.Add(new Vector2Int(xx, yy));
+                    queue.Enqueue(new Vector2Int(xx, yy));
                 }
             }
             return DeleteNonCircle(res);
@@ -72,29 +75,40 @@ namespace ROOT.Signal
 
         private FirewallCircle DeleteNonCircle(FirewallCircle now)
         {
-            var res = new FirewallCircle();
-            var edge = new Dictionary<Vector2Int, List<Vector2Int>>();
+            edge = new Dictionary<Vector2Int, List<Vector2Int>>();
             var inDegree = new Dictionary<Vector2Int, int>();
             foreach (var point in now)
             {
                 edge[point] = new List<Vector2Int>();
-                for (int i = 0; i < 8; ++i)
+                for (var i = 0; i < 8; ++i)
                 {
                     inDegree[point] = 0;
                     int xx = point.x + dx8[i], yy = point.y + dy8[i];
-                    if (xx >= 0 && xx < N && yy >= 0 && yy < N && _board[xx, yy] == 2)
-                    {
-                        ++inDegree[point];
-                        edge[point].Add(new Vector2Int(xx,yy));
-                    }
+                    if (xx < 0 || xx >= N || yy < 0 || yy >= N || _board[xx, yy] != 2)
+                        continue;
+                    ++inDegree[point];
+                    edge[point].Add(new Vector2Int(xx, yy));
                 }
             }
 
-            foreach (var tmp in inDegree)
+            var queue = new Queue<Vector2Int>();
+            foreach (var tmp in inDegree.Where(tmp => tmp.Value == 1))
+                queue.Enqueue(tmp.Key);
+            while (queue.Count != 0)
             {
-                
+                var degreeIsOne = queue.Dequeue();
+                _board[degreeIsOne.x, degreeIsOne.y] = -1;
+                if (inDegree[degreeIsOne] != 1)
+                    continue;
+                foreach (var tmp in edge[degreeIsOne])
+                {
+                    --inDegree[tmp];
+                    if (inDegree[tmp] == 1)
+                        queue.Enqueue(tmp);
+                }
             }
-            return res;
+
+            return now.Where(point => _board[point.x, point.y] == 2).ToList();
         }
         private List<FirewallCircle> DeleteWhiteSpace()
         {
@@ -116,6 +130,69 @@ namespace ROOT.Signal
                         res.Add(GetConnectComponent(i, j));
             return res;
         }
+
+        private void dfs(Vector2Int u, Vector2Int f)
+        {
+            int child = 0;
+            dfn[u] = low[u] = ++dfsClock;
+            foreach (var v in edge[u])
+            {
+                if (dfn[v] == 0) //时间截为零，说明没被访问过 
+                {
+                    dfs(v, u);
+                    ++child;
+                    low[u] = Math.Min(low[v], low[u]); //更新当前顶点cur能否访问到最早顶点的时间截 
+                    if (low[v] >= dfn[u] && f.Equals(new Vector2Int(-1, -1)))
+                        cutVertexSet.Add(u); //v往回找到的最大时间截所在点还在u的子树里，则为u割点 
+                }
+                else
+                {
+                    low[u] = Math.Min(low[u], dfn[v]); //如果顶点u曾经被访问过，更新u的low值 
+                }
+            }
+
+            if (f.Equals(new Vector2Int(-1, -1)) && child == 1 && cutVertexSet.Contains(u)) cutVertexSet.Remove(u); //源点就一条出边，一定不是割点 
+        }
+
+        private List<FirewallCircle> DeleteCutVertex(List<FirewallCircle> testList)
+        {
+            var res = new List<FirewallCircle>();
+            foreach (var test in testList)
+            {
+                edge = new Dictionary<Vector2Int, List<Vector2Int>>();
+                low = new Dictionary<Vector2Int, int>();
+                cutVertexSet = new HashSet<Vector2Int>();
+                dfn = new Dictionary<Vector2Int, int>();
+                dfsClock = 0;
+                foreach (var point in test)
+                {
+                    edge[point] = new List<Vector2Int>();
+                    low[point] = 0;
+                    dfn[point] = 0;
+                    for (var i = 0; i < 8; ++i)
+                    {
+                        int xx = point.x + dx8[i], yy = point.y + dy8[i];
+                        if (xx < 0 || xx >= N || yy < 0 || yy >= N || _board[xx, yy] != 2)
+                            continue;
+                        edge[point].Add(new Vector2Int(xx, yy));
+                    }
+                }
+
+                foreach (var point in test.Where(point => dfn[point] == 0))
+                {
+                    dfs(point, new Vector2Int(-1, -1));
+                }
+
+                if (cutVertexSet.Count == 0)
+                {
+                    res.Add(test);
+                    continue;
+                }
+                var tmpList = test.ToList();
+
+            }
+            return res;
+        }
         private void updateFireWallCircle(Unit[] units)
         {
             //TODO 
@@ -125,7 +202,7 @@ namespace ROOT.Signal
             foreach (var unit in units)
                 _board[unit.CurrentBoardPosition.x, unit.CurrentBoardPosition.y] = 1;
 
-            _connectComponent = DeleteWhiteSpace();
+            _connectComponent = DeleteCutVertex(DeleteWhiteSpace());
 
             _firewallCircle = new FirewallCircle(); //往这个函数里面填东西。
 
@@ -136,7 +213,7 @@ namespace ROOT.Signal
             var data = new Unit[0];
             try
             {
-                SignalMasterMgr.Instance.Paths[SignalType.Firewall].ForEach(u => data.AddRange(u.Where(u0=>u0.UnitHardware==HardwareType.Field)));
+                SignalMasterMgr.Instance.Paths[SignalType.Firewall].ForEach(u => data.AddRange(u.Where(u0 => u0.UnitHardware == HardwareType.Field)));
             }
             catch (KeyNotFoundException)
             {
